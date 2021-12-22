2b340909e13d77963a1d216a5c2d72eae3fbee86
erfaenda <a011787b>
2021-12-22 16:58:50 +0500

    Просто что то

--- chjs/pas_chjs/views.py
+++ chjs/pas_chjs/views.py
@@ -5,8 +5,12 @@ from .models import *
 
 def index(request):
     pas_chjs = Pas_chjs.objects.all()
+    mkr_name = Mkr_name.objects.all()
+    materials_walls = Materials_walls.objects.all()
     context = {
         'pas_chjs': pas_chjs,
-        'title': 'Список паспортов ЧЖС'
+        'title': 'Список паспортов ЧЖС',
+        'mkr_name': mkr_name,
+        'materials_wals': materials_walls,
     }
     return render(request, 'pas_chjs/index.html', context)
