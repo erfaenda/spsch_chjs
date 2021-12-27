886b8891393a3e62dde7a01b4bca9d40906d3320
erfaenda <a011787b>
2021-12-27 15:24:56 +0500

    Пишу свою форму ввода

--- chjs/pas_chjs/forms.py
+++ chjs/pas_chjs/forms.py
@@ -2,9 +2,15 @@ from django import forms
 from .models import *
 
 class PasForm(forms.Form):
-    pas_number = forms.IntegerField(label='Номер паспорта объекта', widget=forms.NumberInput)
-    mkr_name_pas = forms.ModelChoiceField(label='Район', empty_label='Выберите район', queryset=Mkr_name.objects.all())
-    pas_adress = forms.CharField(label='Адрес', max_length=350)
+    pas_number = forms.IntegerField(label='Номер паспорта объекта', widget=forms.NumberInput(attrs={"class": "form-control"}))
+    mkr_name_pas = forms.ModelChoiceField(label='Район', empty_label='Выберите район', queryset=Mkr_name.objects.all(), widget=forms.Select(attrs={"class": "form-control"}))
+    pas_adress = forms.CharField(label='Адрес', max_length=350, widget=<div class="mb-3">
+        <label for="{{ form.mkr_name_pas.id_for_lable }}" class="form-label">Район:</label>
+        {{ form.mkr_name_pas }}
+        <div class="invalid-feedback">
+            {{ form.mkr_name_pas.errors }}
+        </div>
+    </div>)
     stages = forms.IntegerField(label='Этажей')
     walls_material = forms.ModelChoiceField(label='Материал стен', empty_label='Выберите материал', queryset=Materials_walls.objects.all())
     type_heat = forms.ModelChoiceField(label='Тип отопления', empty_label=' Выберите тип', queryset=Type_heat.objects.all())
