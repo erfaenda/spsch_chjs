0e808929cda5a623f303564eecbd26da2f9f3f35
erfaenda <a011787b>
2021-12-28 13:28:08 +0500

    Форма тяп ляп но уже внешне как прототип пойдет

--- chjs/pas_chjs/forms.py
+++ chjs/pas_chjs/forms.py
@@ -4,26 +4,20 @@ from .models import *
 class PasForm(forms.Form):
     pas_number = forms.IntegerField(label='Номер паспорта объекта', widget=forms.NumberInput(attrs={"class": "form-control"}))
     mkr_name_pas = forms.ModelChoiceField(label='Район', empty_label='Выберите район', queryset=Mkr_name.objects.all(), widget=forms.Select(attrs={"class": "form-control"}))
-    pas_adress = forms.CharField(label='Адрес', max_length=350, widget=<div class="mb-3">
-        <label for="{{ form.mkr_name_pas.id_for_lable }}" class="form-label">Район:</label>
-        {{ form.mkr_name_pas }}
-        <div class="invalid-feedback">
-            {{ form.mkr_name_pas.errors }}
-        </div>
-    </div>)
-    stages = forms.IntegerField(label='Этажей')
-    walls_material = forms.ModelChoiceField(label='Материал стен', empty_label='Выберите материал', queryset=Materials_walls.objects.all())
-    type_heat = forms.ModelChoiceField(label='Тип отопления', empty_label=' Выберите тип', queryset=Type_heat.objects.all())
+    pas_adress = forms.CharField(label='Адрес', max_length=350, widget=forms.TextInput(attrs={"class": "form-control"}))
+    stages = forms.IntegerField(label='Этажей', widget=forms.NumberInput(attrs={"class": "form-control"}))
+    walls_material = forms.ModelChoiceField(label='Материал стен', empty_label='Выберите материал', queryset=Materials_walls.objects.all(), widget=forms.Select(attrs={"class": "form-control"}))
+    type_heat = forms.ModelChoiceField(label='Тип отопления', empty_label=' Выберите тип', queryset=Type_heat.objects.all(), widget=forms.Select(attrs={"class": "form-control"}))
     electric = forms.BooleanField(label='Электрофицирован?', initial=True)
-    apis = forms.IntegerField(label='Сколько АПИ?')
-    apis_works = forms.IntegerField(label='Исправных АПИ')
-    living = forms.IntegerField(label='Проживают')
-    childrens = forms.IntegerField(label='Дети')
-    characteristic = forms.CharField(label='Краткая характеристика проживающих', required=False, widget=forms.Textarea)
-    '''owner_name = forms.CharField(max_length=350)
-    date_owner = models.DateField(verbose_name='Дата')
-    woker_mchs = forms.CharField(max_length=350)
-    woker_date = models.DateField(verbose_name='Дата')
-    unfineshed = forms.BooleanField()
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    apis = forms.IntegerField(label='Сколько АПИ?', widget=forms.NumberInput(attrs={"class": "form-control"}))
+    apis_works = forms.IntegerField(label='Исправных АПИ', widget=forms.NumberInput(attrs={"class": "form-control"}))
+    living = forms.IntegerField(label='Проживают', widget=forms.NumberInput(attrs={"class": "form-control"}))
+    childrens = forms.IntegerField(label='Дети', widget=forms.NumberInput(attrs={"class": "form-control"}))
+    characteristic = forms.CharField(label='Характеристика проживающих', required=False, widget=forms.Textarea(attrs={"class": "form-control", "rows": 5}))
+    owner_name = forms.CharField(label='Собственник', max_length=350, widget=forms.TextInput(attrs={"class": "form-control"}))
+    date_owner = forms.DateField(label='Дата', widget=forms.DateInput(attrs={"class": "form-control"}))
+    woker_mchs = forms.CharField(label='Сотрудник проводивший профобследование', max_length=350, widget=forms.TextInput(attrs={"class": "form-control"}))
+    woker_date = forms.DateField(label='Дата', widget=forms.DateInput(attrs={"class": "form-control"}))
+    unfineshed = forms.BooleanField(label='Не достроен?', initial=True)
+    '''created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
     created_update = models.DateTimeField(auto_now=True, verbose_name='Обновлено')'''
